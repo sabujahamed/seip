b2c6d7d34a4936b59788d985ad3a0b077394fe9d
sabujahamed <sabujahamed92@gmail.com>
2015-06-02 11:31:12 +0600

    First Commit

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            
+            
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
